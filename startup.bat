7aaa56aee167b6e5135645f20821f8c1dee23802
Grechyshkin Danil <danil.grechyshkin@extern.a1.at>
2023-03-22 21:43:06 +0200

    [venom] MANIFEST.MF

--- startup.bat
+++ startup.bat
@@ -1,9 +1,10 @@
 :: make sure that all modules ready to be executed
 
 ::1. start port hub
-java -jar out/artifacts/venom_jar/venom.jar ua.dz.venommath.VenomMathApplication
+start java -jar out/artifacts/venom_jar/venom.jar ua.dz.venommath.VenomMathApplication
 
 ::2. start instances
+
 
 ::4. start port Hub
 java -jar out/artifacts/portHub_jar/portHub.jar ua.dz.porthub.PortHubApplication
