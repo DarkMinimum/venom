2e2513915efd893ac1d25b7810568c2ecf3555a4
Grechyshkin Danil <danil.grechyshkin@extern.a1.at>
2023-03-22 21:54:55 +0200

    [venom] startup.bat

--- startup.bat
+++ startup.bat
@@ -1,10 +1,15 @@
 :: make sure that all modules ready to be executed
 
-::1. start port hub
+:: 1. start nested services
+
+:: venomCS
+:: ...
+
+:: venomMath
 start java -jar out/artifacts/venom_jar/venom.jar ua.dz.venommath.VenomMathApplication
 
-::2. start instances
+:: venomPy
+start python .\venomPy\main.py
 
-
-::4. start port Hub
+:: 2. start port Hub
 java -jar out/artifacts/portHub_jar/portHub.jar ua.dz.porthub.PortHubApplication
